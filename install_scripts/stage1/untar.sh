1eab3635e3834fdc997f8eb35ddb63740acff7aa
R.J. Keller <rjkeller@pixonite.com>
2017-09-09 18:09:00 -0700

    looks like the stage1 download server has cert problems

--- install_scripts/stage1/untar.sh
+++ install_scripts/stage1/untar.sh
@@ -1,3 +1,3 @@
-wget https://www.gentoofan.org/gentoo/stage1/stage1-amd64-polyc-015.tar.bz2
+wget https://www.gentoofan.org/gentoo/stage1/stage1-amd64-polyc-015.tar.bz2 --no-check-certificate
 tar xjpf stage1*.tar.bz2
 rm -rf stage1*.tar.bz2
